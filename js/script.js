98a968e7a16735c4409240516c4087089bd366bd
Patricia Moraru <patriciamoraruv@gmail.com>
2026-03-07 12:53:14 +0200

    feat: add implementation of a sticky navigation bar

--- js/script.js
+++ js/script.js
@@ -12,3 +12,26 @@ btnNavEl.addEventListener("click", function() {
   headerEl.classList.toggle("nav-open");
 });
   
+// Sticky navigation
+const sectionHeroEl = document.querySelector(".section-hero");
+
+if (sectionHeroEl) {
+  const headerHeight = headerEl.getBoundingClientRect().height;
+
+  const obs = new IntersectionObserver(
+    function (entries) {
+      const ent = entries[0];
+      console.log(ent);
+
+      if (!ent.isIntersecting) document.body.classList.add("sticky");
+      else document.body.classList.remove("sticky");
+    },
+    {
+      root: null,
+      threshold: 0,
+      rootMargin: `-${headerHeight}px`,
+    }
+  );
+
+  obs.observe(sectionHeroEl);
+}
